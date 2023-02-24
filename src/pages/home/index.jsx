b6186b841ef8d2c6427812546cba0b8f80863995
Micaela Pinheiro <micaela.mews@gmail.com>
2023-02-24 17:24:46 -0300

    set the home container to justify-center

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,7 +3,7 @@ import { ClosedSales } from "../../componets/closedSales"
 
 export function Home() {
     return (
-        <div className="flex flex-col md:flex-row items-center md:items-start w-screen h-screen py-10 px-10 m-auto bg-background-color text-white">
+        <div className="flex flex-col md:flex-row justify-center md:items-start w-screen h-screen py-10 px-10 m-auto bg-background-color text-white">
         <div className="flex flex-col md:flex-row gap-4">
             <Overview />
             <ClosedSales />
