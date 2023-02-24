5836d6aac6a0f10b9980dc5e8511585fbf18eed8
Micaela Pinheiro <micaela.mews@gmail.com>
2023-02-24 20:21:22 -0300

    create monthly goal card

--- src/componets/monthlyGoal/index.jsx
+++ src/componets/monthlyGoal/index.jsx
@@ -1,17 +1,18 @@
 import { CircularProgressbarWithChildren } from "react-circular-progressbar"
 import { Card } from "../card"
 
-export function ClosedSales() {
-    function cardChildren() {
-        const valueProgress = 70
+export function MonthlyGoal() {
+    function cardChildren(){
+        const valueProgress = 90
         return (
             <div className="flex flex-col gap-8">
                 <div className="w-fit m-auto">
-                    <CircularProgressbarWithChildren className='w-{197px} h-[197px] stroke-purple' value={valueProgress} strokeWidth={15}>
+                    <CircularProgressbarWithChildren className='w-{197px} h-[197px] stroke-origen' value={valueProgress} strokeWidth={15}>
                         <div className='text-[34px] leading-[160%] font-bold'>{valueProgress}%</div>
                         <div className="text-base  leading-[160%]">alcançada</div>
                     </CircularProgressbarWithChildren>
                 </div>
+                
                 <div className="flex gap-5 text-[14px]">
                     <div className="flex items-center">
                         <span className="inline-block w-[15px] h-[15px] mr-1 bg-dark-purple rounded-full"></span>
@@ -19,7 +20,7 @@ export function ClosedSales() {
                         <span className="pl-[10px]">100</span> 
                     </div>
                     <div className="flex items-center">
-                        <span className="inline-block w-[15px] h-[15px] mr-1 bg-purple rounded-full"></span>
+                        <span className="inline-block w-[15px] h-[15px] mr-1 bg-origen rounded-full"></span>
                         <span className="font-medium">Alcandado</span>
                         <span className="pl-[10px]">70</span> 
                     </div>
@@ -29,7 +30,7 @@ export function ClosedSales() {
     }
     return (
         <div>
-            <Card title='Vendas fechadas' elementChildren={cardChildren()} />
+            <Card title='Metal mensal' elementChildren={cardChildren()}/>
         </div>
     )
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,14 @@
 import { Overview } from "../../componets/overview"
 import { ClosedSales } from "../../componets/closedSales"
+import { MonthlyGoal } from "../../componets/monthlyGoal"
 
 export function Home() {
     return (
-        <div className="flex flex-col md:flex-row justify-center md:items-start w-screen h-screen py-10 px-10 m-auto bg-background-color text-white">
+        <div className="flex flex-col md:flex-row justify-center md:items-start w-screen min-h-screen py-10 px-10 m-auto bg-background-color text-white">
         <div className="flex flex-col md:flex-row gap-4">
             <Overview />
             <ClosedSales />
+            <MonthlyGoal />
         </div>
         </div>
     )
